8431a7c094c3c9668bbe22b9d412437cd5e236e1
liweihuang <liweihuang81@camelotchina.com>
2016-03-21 18:36:03 +0800

    aaa

--- account-captcha/src/main/java/com/boodman/mvntest/account/impl/AccountCaptchaServiceImpl.java
+++ account-captcha/src/main/java/com/boodman/mvntest/account/impl/AccountCaptchaServiceImpl.java
@@ -1,43 +1,92 @@
 package com.boodman.mvntest.account.impl;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.InitializingBean;
 
 import com.boodman.mvntest.account.api.AccountCaptchaException;
 import com.boodman.mvntest.account.api.AccountCaptchaService;
+import com.boodman.mvntest.account.util.CaptchaUtils;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.util.Config;
 
 public class AccountCaptchaServiceImpl implements AccountCaptchaService,InitializingBean {
 
+	private Map<String, String> captchaMap = new HashMap<String, String>();
+
+
+	private List<String> perDefinedTexts;
+	private int textCount=0;
+	private DefaultKaptcha producer;
+	
+	/**
+	 * 生成验证码key
+	 */
 	public String generateCaptchaKey() throws AccountCaptchaException {
 	
-		return null;
+		//获取随机key
+		String key  = CaptchaUtils.getRandomString();
+		
+		String value = getCaptchaText();
+		//写入key，value
+		captchaMap.put(key, value);
+		return key;
 	}
-
+	
+	/**
+	 * 生成验证码图片
+	 */
 	public byte[] generateCaptchaImage(String captchaKey) throws AccountCaptchaException {
-		// TODO Auto-generated method stub
-		return null;
+		String text = captchaMap.get(captchaKey);
+		//如果与key对应的value为空
+		if (text==null) {
+			throw new AccountCaptchaException("Captchakey", captchaKey);
+		}
+		//生成验证码图片
+		BufferedImage image = producer.createImage(text);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		try {
+			//将图片输出到out.jpg中
+			ImageIO.write(image, "jpg", out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return out.toByteArray();
 	}
 
+	//验证验证码是否正确
 	public boolean validateCaptcha(String captchaKey, String captchaValue) throws AccountCaptchaException {
-		// TODO Auto-generated method stub
+		String text = captchaMap.get(captchaKey);
+		if (text==null) {
+			throw new AccountCaptchaException("", "");
+		}
+		if (text.equals(captchaValue)) {
+			captchaMap.remove(captchaKey);
+		}else {
+			return false;
+		}
 		return false;
 	}
 
 	public List<String> getPreDefinedTexts() {
-		// TODO Auto-generated method stub
-		return null;
+		return perDefinedTexts;
 	}
 
 	public void setPreDefinedTexts(List<String> preDefinedTexts) {
-		// TODO Auto-generated method stub
+		this.perDefinedTexts = preDefinedTexts;
 
 	}
 
-	private DefaultKaptcha producer;
+
 	/**
 	 * 初始化验证码生成器
 	 */
@@ -46,4 +95,18 @@ public class AccountCaptchaServiceImpl implements AccountCaptchaService,Initiali
 		producer.setConfig(new Config(new Properties()));
 	}
 
+	/**
+	 * 创建验证码内容
+	 * @return
+	 */
+	public String getCaptchaText(){
+		if (perDefinedTexts!=null&&getPreDefinedTexts().isEmpty()) {
+			String text = getPreDefinedTexts().get(textCount);
+			textCount = (textCount+1) % getPreDefinedTexts().size();
+			return text;
+		}else {
+			return producer.createText();
+		}
+		
+	}
 }
--- account-captcha/src/main/java/com/boodman/mvntest/account/util/CaptchaUtils.java
+++ account-captcha/src/main/java/com/boodman/mvntest/account/util/CaptchaUtils.java
@@ -9,8 +9,10 @@ public class CaptchaUtils {
 		Random random = new Random();
 		StringBuffer result = new StringBuffer();
 		for (int i = 0; i < 8; i++) {
+			//获取随机字符
 			char chars = range.charAt(random.nextInt(range.length()));
 			System.out.println(chars);
+			//拼接字符
 			result.append(chars);
 		}
 		System.out.println(result.toString());
